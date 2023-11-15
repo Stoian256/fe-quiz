5d8ef36f66b6899de728722859249d06991e5976
Claudiu Sonica <csonica95@gmail.com>
2023-11-15 15:40:03 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-/* eslint-disable no-alert */
-import { twMerge } from "tailwind-merge";
-import { Button } from "@shadcn/components/ui/button";
+// import { twMerge } from "tailwind-merge";
+// import { Button } from "@shadcn/components/ui/button";
 
 const App = () => (
-  <div
-    className={`bg-gray-100 h-screen flex flex-col items-center justify-center gap-y-14 px-10 ${twMerge(
-      "lg:px-18",
-      "lg:px-20"
-    )}`}
-  >
-    <p className="text-6xl lg:text-8xl text-blue-900 font-bold text-center">
-      Quiz Project
-    </p>
-    <Button onClick={() => alert("You just clicked a button")}> 
-      Click me!
-    </Button>
+  <div>
+    
   </div>
 );
 
