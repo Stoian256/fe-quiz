53890b36da64cd22cbb268c3608e9ddf052b9bca
Lavinia Tinca <lavinia.tinca@ddroidd.com>
2023-11-17 12:45:05 +0200

    logo file name changed

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,7 +2,7 @@ import logo from "./../assets/header-logo-white.svg";
 
 const Logo = () => {
   return (
-    <div className="p-6 pl-2">
+    <div className="p-6 pl-2 mr-0">
       <img src={logo} alt={logo} />
     </div>
   );
