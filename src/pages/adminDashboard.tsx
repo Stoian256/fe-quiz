53064eca0de68109f5c25e2bf49917eeecb6926c
Talida Ganciu <talida.ganciu@ddroidd.com>
2023-11-17 15:39:24 +0200

    merging branch with main

--- src/pages/adminDashboard.tsx
+++ src/pages/adminDashboard.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import DisplayFilters from "../components/displayFilters";
+import DisplayQuestions from "../components/displayQuestions";
 
 const Dashboard = () => {
   const navigate = useNavigate();
@@ -9,7 +11,12 @@ const Dashboard = () => {
       state: { mainTitle: "Dashboard Title", title: "dashboard second title" }
     });
   }, [navigate]);
-  return <div className="text-center">Dashboard content goes here</div>;
+  return (
+    <div className="text-center">
+      <DisplayFilters />
+      <DisplayQuestions />
+    </div>
+  );
 };
 
 export default Dashboard;
