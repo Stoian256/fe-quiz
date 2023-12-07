dbd15971447f3a83143aebd1ccc9f3b7d8638fad
Talida Ganciu <talida.ganciu@ddroidd.com>
2023-12-07 18:49:52 +0200

    feat: updated the search quizz based on new endpoint and small UX improvements

--- src/components/searchQuizz/searchQuizzCard.tsx
+++ src/components/searchQuizz/searchQuizzCard.tsx
@@ -1,37 +1,32 @@
+import { Quizz } from "@shadcn/utils/interfaces/Quizz";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 
-interface SearchQuizzCardProps {
-  title: string;
-  tags: string[];
-  difficulty: string;
-  timeLimit: number;
-}
-
-const SearchQuizzCard: React.FC<SearchQuizzCardProps> = ({
-  title,
-  tags,
-  difficulty,
-  timeLimit
+const SearchQuizzCard: React.FC<Quizz> = ({
+  difficultyLevel,
+  quizTags,
+  quizTitle,
+  timeLimitMinutes
 }) => {
   return (
     <div className="border border-dashed p-5 my-3 flex flex-col gap-2">
-      <h3 className="text-3xl">{title}</h3>
+      <h3 className="text-3xl">{quizTitle}</h3>
       <div className="flex gap-4">
-        {tags.map((tag, index) => (
+        {quizTags.map((tag, index) => (
           <Badge
             key={`${index}-${tag}`}
             className="rounded-none text-lg bg-gray-300 text-black font-bold"
           >
-            {tag}
+            {tag.tagTitle}
           </Badge>
         ))}
       </div>
       <p className="text-lg text-gray-500">
-        Difficulty: <span className="font-bold">{difficulty}</span>
+        Difficulty: <span className="font-bold">{difficultyLevel}</span>
       </p>
       <p className="text-lg  text-gray-500">
-        Time Limit: <span className="font-bold">{timeLimit} minutes</span>
+        Time Limit:{" "}
+        <span className="font-bold">{timeLimitMinutes} minutes</span>
       </p>
       <Button className="bg-yellow-400 text-white rounded-none hover:bg-yellow-500 p-2 px-10 shadow-lg self-end">
         Start solving
